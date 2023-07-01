37f9956930501aecf1b043a3cac3f83c708e8d0a
Arshad4610 <134123830+Arshad4610@users.noreply.github.com>
2023-07-01 09:33:02 +0530

    Add files via upload

---
+++ p5/exqty.sql
@@ -0,0 +1,17 @@
+set serveroutput on
+declare
+id inven.pid%type;
+invalid_qty exception;
+begin
+	select pid into id from inven where qty<0;
+	if sql%found then raise invalid_qty;
+	end if;
+exception
+	when no_data_found then
+	dbms_output.put_line('NO DATA FOUND');
+	when too_many_rows then
+	dbms_output.put_line('more than 1 row selected');
+	when invalid_qty then
+	dbms_output.put_line('INVALID QUANTITY');
+	update inven set qty=0 where pid=id;
+end;
